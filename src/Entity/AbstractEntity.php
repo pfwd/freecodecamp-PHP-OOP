4ab40091fab6b3f2b7fb8b40dcebdb7eeae7a1db
peter fisher <hello@websomatic.co.uk>
2019-06-11 07:54:09 +0100

    Updates

---
+++ src/Entity/AbstractEntity.php
@@ -0,0 +1,78 @@
+<?php
+namespace Entity;
+
+use DateTime;
+
+abstract class AbstractEntity
+{
+    /**
+     * @var null|int
+     */
+    protected $id;
+
+    /**
+     * @var null|DateTime
+     */
+    protected $dateCreated;
+
+    /**
+     * @var null|DateTime
+     */
+    protected $dateUpdated;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return AbstractEntity
+     */
+    public function setId(?int $id): AbstractEntity
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getDateCreated(): ?DateTime
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param DateTime|null $dateCreated
+     * @return AbstractEntity
+     */
+    public function setDateCreated(?DateTime $dateCreated): AbstractEntity
+    {
+        $this->dateCreated = $dateCreated;
+        return $this;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getDateUpdated(): ?DateTime
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * @param DateTime|null $dateUpdated
+     * @return AbstractEntity
+     */
+    public function setDateUpdated(?DateTime $dateUpdated): AbstractEntity
+    {
+        $this->dateUpdated = $dateUpdated;
+        return $this;
+    }
+
+
+}
---
+++ src/Repository/Type/Invoice.php
@@ -0,0 +1,43 @@
+<?php
+namespace Repository\Type;
+
+use Repository\AbstractRepository;
+
+class Invoice extends AbstractRepository
+{
+    /**
+     * @return mixed
+     */
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function findOne(int $id)
+    {
+
+    }
+
+    /**
+     * @param array $options
+     * @return mixed
+     */
+    public function findOneBy(array $options)
+    {
+        // TODO: Implement findOneBy() method.
+    }
+
+    /**
+     * @param array $options
+     * @return mixed
+     */
+    public function findAllBy(array $options)
+    {
+        // TODO: Implement findAllBy() method.
+    }
+
+}
