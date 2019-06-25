0ea99273c4fdf1369cfb5445af197f006233af1e
peterfisher <c3T-y7J-vo3-qpg>
2019-06-25 07:44:58 +0100

    Adding config path

--- app/config/config.php
+++ app/config/config.php
@@ -4,3 +4,4 @@ define('BASE_PATH', $basePath);
 define('PUBLIC_PATH', $_SERVER['DOCUMENT_ROOT'].'/');
 define('KERNEL_PATH', BASE_PATH.'src/Helper/Kernel/');
 define('TEMPLATE_PATH', PUBLIC_PATH.'views/templates/');
+define('CONFIG_PATH', BASE_PATH.'app/config/');
