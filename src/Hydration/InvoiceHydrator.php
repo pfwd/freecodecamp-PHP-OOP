233ea72040977ef8b510e706c963c3439cae576f
peter fisher <hello@websomatic.co.uk>
2019-07-19 07:48:00 +0100

    Added Invoice Hydrator

---
+++ src/Hydration/InvoiceHydrator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Hydration;
+
+use App\Entity\Type\Invoice;
+
+class InvoiceHydrator extends AbstractHydrator
+{
+    public static function hydrate(array $data): Invoice
+    {
+        $entity = new Invoice();
+        $entity->setReference($data['reference'])
+            ->setTotal($data['total'])
+            ->setVAT($data['vat'])
+        ;
+        parent::hydrateRest($data, $entity);
+
+        return $entity;
+
+    }
+
+}
---
+++ tests/unit/Hydrator/InvoiceTest.php
@@ -0,0 +1,48 @@
+<?php namespace Hydrator;
+
+use App\Entity\Type\Invoice;
+use App\Hydration\InvoiceHydrator;
+use DateTime;
+
+class InvoiceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * @group hydrator
+     * @group hydrator-invoice
+     */
+    public function testHydration()
+    {
+
+        $date = new DateTime();
+        $data = [
+            'id' => 1,
+            'reference' => 'invoice-001',
+            'total' => 2.99,
+            'vat' => 1.25,
+            'date_created' => $date->format(DateTime::ISO8601),
+            'date_updated' => $date->format(DateTime::ISO8601)
+        ];
+        $entity = InvoiceHydrator::hydrate($data);
+
+        $this->assertInstanceOf(Invoice::class, $entity);
+        $this->assertSame(1, $entity->getId());
+        $this->assertSame('invoice-001', $entity->getReference());
+        $this->assertSame(2.99, $entity->getTotal());
+        $this->assertSame(1.25, $entity->getVAT());
+        $this->assertInstanceOf(DateTime::class, $entity->getDateCreated());
+        $this->assertInstanceOf(DateTime::class, $entity->getDateUpdated());
+    }
+}
